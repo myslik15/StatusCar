7eb8d07e1d1718f29617d606ae1f693f13c61e14
Егор Мысливчик <egormyslivchik@MacBook-Air-Egor.local>
2025-10-25 14:01:00 +0300

    Модели машин, объявления и юзер

--- cars/models.py
+++ cars/models.py
@@ -3,8 +3,6 @@ from users.models import User
 
 class Car(models.Model):
     title = models.CharField(verbose_name="Название")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
     brand = models.CharField(verbose_name="Марка")
     model = models.CharField(verbose_name="Модель")
     generation = models.CharField(verbose_name="Поколение")
@@ -15,14 +13,35 @@ class Car(models.Model):
     body_type = models.CharField(verbose_name="Тип кузова")
     drive_type = models.CharField(verbose_name="Привод")
     mileage = models.IntegerField(verbose_name="Пробег")
-    color = models.CharField(verbose_name="Цвет")    
-    damage = models.BooleanField(verbose_name="Есть повреждения")
-    damage_description = models.TextField(blank=True, verbose_name="Описание повреждений")
+    color = models.CharField(verbose_name="Цвет")
+
 
     class Meta:
         verbose_name = "Автомобиль"
         verbose_name_plural = "Автомобили"
-        ordering = ['-created_at']
+
     
     def __str__(self):
         return f"{self.brand} {self.model} {self.year}."
+
+
+class Advertisement(models.Model):
+    car = models.OneToOneField(Car, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo_1 = models.ImageField(upload_to='???')
+    photo_2 = models.ImageField(upload_to='???', blank=True, null=True)
+    photo_3 = models.ImageField(upload_to='???', blank=True, null=True)
+    photo_4 = models.ImageField(upload_to='???', blank=True, null=True)
+    damage = models.BooleanField(verbose_name="Есть повреждения")
+    damage_description = models.TextField(blank=True, verbose_name="Описание повреждений")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+
+
+
+
+
+
